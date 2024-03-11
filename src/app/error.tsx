ccba9a1237962a3ab45100df1876437d21ea9ddc
Charles Randicha <charles.randicha@gmail.com>
2024-03-11 18:11:09 +0700

    feat: refine not found page and error page style

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,65 @@
 'use client'
 
-import React from 'react'
-
-import { default as ErrorComponent } from 'components/Error'
+import React, { useEffect } from 'react'
 
 export type TErrorProps = {
   error: Error
   reset: () => void
 }
 
-const ErrorPage = (props: TErrorProps) => {
-  return <ErrorComponent {...props} />
+const ErrorPage = ({ error, reset }: TErrorProps) => {
+  const doContactSupport = () => {
+    // add code for contact support here
+  }
+
+  const doPostLog = () => {
+    // add code for posting logs here
+  }
+
+  const isProduction = process.env.NODE_ENV === 'production'
+
+  useEffect(() => {
+    doPostLog()
+  }, [])
+  return (
+    <div className="flex h-full items-center justify-center">
+      <section className="flex h-full rounded-md border border-gray-300 bg-white shadow-lg md:h-auto">
+        <div className="flex w-screen flex-col justify-center gap-4 p-6 md:w-[800px]">
+          <h1 className="text-center text-2xl">
+            Oops! A client-side error has occurred.
+          </h1>
+          <p>Don&apos;t worry, you can:</p>
+          <ul className="list-disc">
+            <li>
+              Try resetting the segment to its initial state by clicking{' '}
+              <b>Reset</b> Button below
+            </li>
+            <li>Contact our support team for further assistance</li>
+          </ul>
+          <div className="flex w-full flex-row justify-center gap-4 md:w-auto">
+            <button
+              onClick={reset}
+              className="flex-1 rounded-md bg-blue-600 px-4 py-3 text-white hover:scale-105 hover:bg-blue-700 active:scale-95 active:bg-blue-800 md:flex-none"
+            >
+              Reset
+            </button>
+            <button
+              onClick={doContactSupport}
+              className="flex-1 rounded-md border border-black bg-white px-4 py-3 text-black hover:scale-105 hover:bg-gray-100 active:scale-95 active:bg-gray-200 md:flex-none"
+            >
+              Contact Support
+            </button>
+          </div>
+          {!isProduction && (
+            <div className="max-h-[350px] overflow-auto rounded-md bg-black p-4 text-white shadow-md">
+              <pre>{error.message}</pre>
+              <pre>{error.stack}</pre>
+            </div>
+          )}
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default ErrorPage
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 const NotFoundPage = () => {
   return (
     <div className="flex h-full items-center justify-center">
-      <section className="flex w-[500px] flex-col gap-4 rounded-md border border-gray-300 bg-white p-6 shadow-lg">
+      <section className="flex h-full w-full flex-col justify-center gap-4 rounded-md border border-gray-300 bg-white p-6 shadow-lg md:h-auto md:w-[500px]">
         <h1 className="text-center text-2xl">
           Oops! Seems like the page that you are looking for does not exist.
         </h1>
         <div className="flex justify-center">
-          <Link href={'/'}>
-            <button className="rounded-md bg-blue-600 px-4 py-3 text-white hover:scale-105 hover:bg-blue-700 active:scale-95 active:bg-blue-800">
-              Go to Home
+          <Link href={'/'} className="flex w-full justify-center">
+            <button className="w-full rounded-md bg-blue-600 px-4 py-3 text-white hover:scale-105 hover:bg-blue-700 active:scale-95 active:bg-blue-800 md:w-auto">
+              Back to Homepage
             </button>
           </Link>
         </div>

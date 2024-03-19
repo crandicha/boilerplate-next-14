f0e3ea21160f0a6acdc9d94d46cbceede2f5867e
Charles Randicha <charles.randicha@gmail.com>
2024-03-19 09:16:53 +0700

    fix: remove deprecated stylelint config and add some rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-standard-scss'],
   rules: {
     'selector-pseudo-class-no-unknown': [
       true,
@@ -8,13 +8,9 @@ module.exports = {
       },
     ],
     'custom-property-pattern': '^.+$',
-    'selector-class-pattern': [
-      '^([a-z][a-z0-9]*)(_[a-z0-9]+)*$',
-      {
-        message:
-          'Selector should be written in snake_case (e.g. my_class_name)',
-      },
-    ],
     'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind'] }],
+    'property-no-vendor-prefix': null,
+    'value-no-vendor-prefix': null,
+    'color-function-notation': null,
   },
 }
